a3dd3a64c519d35d711df066f6c9ce5d13cdc12b
Eynz6002 <enzoytalosilvapires2006@gmail.com>
2026-01-09 22:24:18 -0300

    modificando os dados do inquilino

--- aluguel/models.py
+++ aluguel/models.py
@@ -5,6 +5,3 @@ from propriedade.models import Propriedade, Usuario
 class Aluguel(models.Model):
     inquilino = models.ForeignKey(Usuario, on_delete=models.CASCADE)
     propriedade = models.ForeignKey(Propriedade, on_delete=models.CASCADE)
-    dias_alugados = models.DateField()
-    total_pago = models.IntegerField()
-    encerrado = models.BooleanField()
